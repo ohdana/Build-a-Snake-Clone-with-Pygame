5528b14526996c716f6bfa964d67f4ffea8db6b0
Dana Goosen <klymentovska@gmail.com>
2025-04-10 10:04:48 +0200

    Snake Movement

--- code/main.py
+++ code/main.py
@@ -17,18 +17,47 @@ class Main:
         self.snake = Snake()
         self.apple = Apple(self.snake.body)
 
+        # timer
+        self.update_event = pygame.event.custom_type()
+        pygame.time.set_timer(self.update_event, 200)
+
     def draw_bg(self):
+        self.display_surface.fill(LIGHT_GREEN)
         for rect in self.bg_rects:
             pygame.draw.rect(self.display_surface, DARK_GREEN, rect)
             
+    def input(self):
+        keys = pygame.key.get_pressed()
+        new_direction = pygame.Vector2(0, 0)
+        if keys[pygame.K_RIGHT]: 
+            self.snake.direction = pygame.Vector2(1, 0) if self.snake.direction.x != -1 else self.snake.direction
+        if keys[pygame.K_LEFT]:
+            self.snake.direction = pygame.Vector2(-1, 0)  if self.snake.direction.x != 1 else self.snake.direction
+        if keys[pygame.K_UP]:
+            self.snake.direction = pygame.Vector2(0, -1)  if self.snake.direction.y != 1 else self.snake.direction
+        if keys[pygame.K_DOWN]: 
+            self.snake.direction = pygame.Vector2(0, 1) if self.snake.direction.y != -1 else self.snake.direction
+    
+    def collision(self):
+        # apple
+        if self.snake.body[0] == self.apple.pos:
+            self.snake.has_eaten = True
+            self.apple.set_pos(self.snake.body)
+
     def run(self):
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     exit()
-            
-            self.display_surface.fill(LIGHT_GREEN)
+                if event.type == self.update_event:
+                    self.snake.update()
+
+            # updates
+            self.input()
+            self.collision()
+
+            # drawing
             self.draw_bg()
             self.snake.draw()
             self.apple.draw()
--- code/snake.py
+++ code/snake.py
@@ -4,6 +4,14 @@ class Snake:
     def __init__(self):
         self.display_surface = pygame.display.get_surface()
         self.body = [pygame.Vector2(START_COL - col, START_ROW) for col in range(START_LENGTH)]
+        self.direction = pygame.Vector2(1, 0)
+        self.has_eaten = False
+
+    def update(self):
+        body_copy = self.body[:] if self.has_eaten else self.body[:-1]
+        body_copy.insert(0, body_copy[0] + self.direction)
+        self.body = body_copy[:]
+        self.has_eaten = False
 
     def draw(self):
         for point in self.body:
